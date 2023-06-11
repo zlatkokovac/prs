361b7afad81d837390da8e1f41bb60a16cecadbb
zlatkokovac <vet.zlatko.kovac@gmail.com>
2023-06-11 15:59:37 -0500

    update algorithm in javaRPS file

--- javaRockPaperScissor.js
+++ javaRockPaperScissor.js
@@ -1,9 +1,20 @@
+//setup variables//
+
 let computerChoice = ''
-let playerChoice
+let playerChoice = ''
+let playerScore = 0
+let computerScore = 0
 
+//ALGORITHM//
 
-// Computer play - random choice
-function compPlay() {
+// 1. computer makes a play => compChoice is defined
+// 2. player make a play => playerChoice is defined
+// 3. compare scores => assign a point to the winner
+// 4. update score
+// 
+
+// Computer play - random choice//
+function computerPlay() {
     let computerChoice = Math.floor(Math.random()*3)+1 
     if (computerChoice ==1) {
             computerChoice = 'rock'
@@ -15,5 +26,7 @@ function compPlay() {
         return computerChoice
 }
 
+console.log("Computer choice was:", computerPlay())
 
-
+// Player play - select options //
+// using prompt()
