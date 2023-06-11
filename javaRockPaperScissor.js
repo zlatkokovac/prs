101afc516e2b4878754de6c63939681133e072fe
zlatkokovac <vet.zlatko.kovac@gmail.com>
2023-06-11 15:19:22 -0500

    Make computerPlay function

--- javaRockPaperScissor.js
+++ javaRockPaperScissor.js
@@ -1,2 +1,19 @@
-let computerChoice
+let computerChoice = ''
 let playerChoice
+
+
+// Computer play - random choice
+function compPlay() {
+    let computerChoice = Math.floor(Math.random()*3)+1 
+    if (computerChoice ==1) {
+            computerChoice = 'rock'
+        } else if (computerChoice ==2) {
+            computerChoice = 'paper'
+        } else if (computerChoice ==3) {
+            computerChoice = 'scissor'
+        }
+        return computerChoice
+}
+
+
+
